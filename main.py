5615060c04de5cfc32231e67073a0d84afcd1c3b
lindozy <492201845@qq.com>
2023-04-11 21:54:56 +0800

    1.修改请求端口

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     uvicorn.run(app='main:app',
                 host='127.0.0.1',
-                port=3002,
+                port=3003,
                 # debug=True,
-                reload=True
+                # reload=True
                 )
