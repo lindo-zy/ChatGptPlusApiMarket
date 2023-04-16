748b8c867154a612d7b9b9c07fc6527ffb36c22c
lindozy <492201845@qq.com>
2023-04-16 22:26:23 +0800

    1.添加4个worker

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     uvicorn.run(app='main:app',
                 host='0.0.0.0',
                 port=3003,
+                workers=4
                 # debug=True,
                 # reload=True
                 )
